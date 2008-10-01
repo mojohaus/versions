721126985bcf922a7bf6923cbd24943446ba2a0b
Stephen Connolly <stephen.alan.connolly@gmail.com>
2008-10-01 17:06:31 +0000

    adding integration tests
    git-svn-id: file:///home/tiste/MOJOHAUS-TO-GIT/SVN-MOJO-WIP/trunk/mojo/versions-maven-plugin@7773 52ab4f32-60fc-0310-b215-8acea882cd1b

---
+++ src/it/dummy-maven-plugin-1.0/src/main/java/org/codehaus/mojo/versions/it/DummyMojo.java
@@ -0,0 +1,69 @@
+package org.codehaus.mojo.versions.it;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.project.MavenProject;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+/**
+ * Abstract base class for Versions Mojos.
+ *
+ * @author <a href="mailto:stephen.alan.connolly@gmail.com">Stephen Connolly</a>
+ * @goal dummy
+ */
+public class DummyMojo
+    extends AbstractMojo
+{
+
+    /**
+     * @parameter expression="${project}"
+     * @required
+     * @readonly
+     * @since 1.0
+     */
+    private MavenProject project;
+
+    public void execute()
+        throws MojoExecutionException, MojoFailureException
+    {
+        getLog().info( "This is version 1.0" );
+        File target = new File( project.getBuild().getDirectory() );
+        getLog().info( project.getBuild().getDirectory() );
+        target.mkdirs();
+        File output = new File( target, "dummy.version" );
+        try
+        {
+            FileWriter out = new FileWriter( output );
+            out.write( "1.0\n" );
+            out.close();
+        }
+        catch ( IOException e )
+        {
+            getLog().warn( e );
+        }
+    }
+
+}
