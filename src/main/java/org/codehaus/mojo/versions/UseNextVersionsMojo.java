c82a80a2258bfc9e2e7c9130e0d88fcf383697d6
Stephen Connolly <stephen.alan.connolly@gmail.com>
2009-04-20 16:42:18 +0000

    adding some more goals to update versions
    git-svn-id: file:///home/tiste/MOJOHAUS-TO-GIT/SVN-MOJO-WIP/trunk/mojo/versions-maven-plugin@9529 52ab4f32-60fc-0310-b215-8acea882cd1b

---
+++ src/main/java/org/codehaus/mojo/versions/UseNextVersionsMojo.java
@@ -0,0 +1,117 @@
+package org.codehaus.mojo.versions;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import org.apache.maven.artifact.Artifact;
+import org.apache.maven.artifact.versioning.ArtifactVersion;
+import org.apache.maven.artifact.versioning.OverConstrainedVersionException;
+import org.apache.maven.model.Dependency;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.codehaus.mojo.versions.api.ArtifactVersions;
+import org.codehaus.mojo.versions.api.PomHelper;
+import org.codehaus.mojo.versions.rewriting.ModifiedPomXMLEventReader;
+
+import javax.xml.stream.XMLStreamException;
+import java.util.Collection;
+import java.util.Iterator;
+
+/**
+ * Replaces any version with the latest version.
+ *
+ * @author Stephen Connolly
+ * @goal use-next-versions
+ * @requiresProject true
+ * @requiresDirectInvocation true
+ * @since 1.0-alpha-3
+ */
+public class UseNextVersionsMojo
+    extends AbstractVersionsDependencyUpdaterMojo
+{
+
+    // ------------------------------ METHODS --------------------------
+
+    /**
+     * @param pom the pom to update.
+     * @throws org.apache.maven.plugin.MojoExecutionException
+     *          when things go wrong
+     * @throws org.apache.maven.plugin.MojoFailureException
+     *          when things go wrong in a very bad way
+     * @throws javax.xml.stream.XMLStreamException
+     *          when things go wrong with XML streaming
+     * @see org.codehaus.mojo.versions.AbstractVersionsUpdaterMojo#update(org.codehaus.mojo.versions.rewriting.ModifiedPomXMLEventReader)
+     */
+    protected void update( ModifiedPomXMLEventReader pom )
+        throws MojoExecutionException, MojoFailureException, XMLStreamException
+    {
+        if ( getProject().getDependencyManagement() != null && isProcessingDependencyManagement() )
+        {
+            useNextVersions( pom, getProject().getDependencyManagement().getDependencies() );
+        }
+        if ( isProcessingDependencies() )
+        {
+            useNextVersions( pom, getProject().getDependencies() );
+        }
+    }
+
+    private void useNextVersions( ModifiedPomXMLEventReader pom, Collection dependencies )
+        throws XMLStreamException, MojoExecutionException
+    {
+        Iterator i = dependencies.iterator();
+
+        while ( i.hasNext() )
+        {
+            Dependency dep = (Dependency) i.next();
+
+            if ( isExcludeReactor() && isProducedByReactor( dep ) )
+            {
+                continue;
+            }
+
+            String version = dep.getVersion();
+            Artifact artifact = this.findArtifact( dep );
+            if ( !isIncluded( artifact ) )
+            {
+                continue;
+            }
+
+            ArtifactVersions versions =
+                getHelper().lookupArtifactVersions( artifact, Boolean.TRUE.equals( allowSnapshots ) );
+            try
+            {
+                ArtifactVersion[] newer = versions.getNewerVersions( artifact.getSelectedVersion(), false );
+                if ( newer.length > 0 )
+                {
+                    if ( PomHelper.setDependencyVersion( pom, dep.getGroupId(), dep.getArtifactId(), version,
+                                                         newer[0].toString() ) )
+                    {
+                        getLog().debug(
+                            "Version set to " + newer[0].toString() + " for dependnecy: " + dep );
+                    }
+                }
+            }
+            catch ( OverConstrainedVersionException e )
+            {
+                getLog().warn( "This should never happen as your build should not work at all if this is thrown", e );
+            }
+        }
+    }
+
+}
