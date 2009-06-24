0047265b803b6a645e022f5163fdd99aa7e4cce3
Stephen Connolly <stephen.alan.connolly@gmail.com>
2009-06-24 16:25:12 +0000

    adding a test to check same error condition as MARTIFACT-18, but for numeric version comparator
    git-svn-id: file:///home/tiste/MOJOHAUS-TO-GIT/SVN-MOJO-WIP/trunk/mojo/versions-maven-plugin@10037 52ab4f32-60fc-0310-b215-8acea882cd1b

---
+++ src/test/java/org/codehaus/mojo/versions/ordering/NumericVersionComparatorTest.java
@@ -0,0 +1,42 @@
+package org.codehaus.mojo.versions.ordering;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import junit.framework.TestCase;
+
+public class NumericVersionComparatorTest
+    extends TestCase
+{
+    private NumericVersionComparator instance = new NumericVersionComparator();
+    
+    
+    
+    public void testSmokes() throws Exception {
+        assertTrue(instance.compare( "1.0.0.0.0", "1.0.0.0.1" ) < 0);
+        assertTrue(instance.compare( "1.0.0.0.0", "2.0.0.0.1" ) < 0);
+        assertTrue(instance.compare( "1.0.0.0.0", "1.0.0.0" ) < 0);
+        assertTrue(instance.compare( "1.0.0.0.0", "1.0.0.0.0" ) == 0);
+        assertTrue(instance.compare( "1.0.0.0", "1.0.0.0.0" ) > 0);
+    }
+    
+    public void testBigValues() throws Exception {
+        assertTrue(instance.compare( "1.92.0", "1.100000000000000000000000.0" ) < 0);
+    }
+}
