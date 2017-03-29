c61837527ffc6a4fd6ea42137a9b22114d7d7ce4
Karl Heinz Marbaise <khmarbaise@apache.org>
2017-03-29 20:13:27 +0200

    Removed superflowous shell script.

--- runits.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-mvn -Prun-its clean verify -Drat.ignoreErrors=true -Dinvoker.test=it-use-latest-versions-001,it-use-latest-versions-002,it-use-latest-versions-003,it-use-latest-versions-004,it-use-latest-versions-005,it-use-latest-versions-006
