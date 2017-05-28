0b8c0a9d42dd87c8b413a34828f7947484835f1d
Karl Heinz Marbaise <khmarbaise@apache.org>
2017-05-28 14:37:00 +0200

    Removed accidently added System.out.*

--- src/main/java/org/codehaus/mojo/versions/api/AbstractVersionDetails.java
+++ src/main/java/org/codehaus/mojo/versions/api/AbstractVersionDetails.java
@@ -191,24 +191,18 @@ public abstract class AbstractVersionDetails
             {
                 continue;
             }
-            System.out.println( "Candidate: " + candidate.toString() );
             int lower = lowerBound == null ? -1 : versionComparator.compare( lowerBound, candidate );
             int upper = upperBound == null ? +1 : versionComparator.compare( upperBound, candidate );
             if ( lower > 0 || upper < 0 )
             {
-                System.out.println( "lower > 0 || upper < 0  lower:" + lower + " upper:" + upper );
                 continue;
             }
             if ( ( !includeLower && lower == 0 ) || ( !includeUpper && upper == 0 ) )
             {
-                System.out.println( "( !includeLower && lower == 0 ) || ( !includeUpper && upper == 0 )  lower:" + lower
-                    + " upper:" + upper );
                 continue;
             }
             if ( !includeSnapshots && ArtifactUtils.isSnapshot( candidate.toString() ) )
             {
-                System.out.println( "!includeSnapshots && ArtifactUtils.isSnapshot( candidate.toString() )  includeSnapshots: "
-                    + includeSnapshots );
                 continue;
             }
             if ( latest == null )
@@ -219,7 +213,6 @@ public abstract class AbstractVersionDetails
             {
                 latest = candidate;
             }
-            System.out.println( "result: " + latest.toString() );
 
         }
         return latest;
