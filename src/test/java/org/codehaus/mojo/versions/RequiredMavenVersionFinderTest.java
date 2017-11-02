7ed4877921cefd2b6d84961a7f47bd3f017d69a2
David Edwards <dje1990uk@gmail.com>
2017-11-02 20:19:39 +0000

    Add 100% unit test coverage for RequiredMavenVersionFinder

--- src/test/java/org/codehaus/mojo/versions/RequiredMavenVersionFinderTest.java
+++ src/test/java/org/codehaus/mojo/versions/RequiredMavenVersionFinderTest.java
@@ -1,14 +1,21 @@
 package org.codehaus.mojo.versions;
 
+import org.apache.maven.artifact.versioning.DefaultArtifactVersion;
+import org.apache.maven.model.Plugin;
+import org.apache.maven.model.PluginExecution;
 import org.apache.maven.model.Prerequisites;
 import org.apache.maven.project.MavenProject;
+import org.codehaus.plexus.util.xml.Xpp3Dom;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
-import static org.junit.Assert.*;
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 import static org.mockito.Mockito.when;
 
 /**
@@ -20,55 +27,286 @@ public class RequiredMavenVersionFinderTest {
     @Mock
     private MavenProject mavenProject;
     @Mock
+    private Prerequisites prerequisites;
+    @Mock
+    private Plugin nonEnforcerPlugin;
+    @Mock
+    private Plugin enforcerPlugin;
+    @Mock
+    private PluginExecution pluginExecution;
+    @Mock
+    private Xpp3Dom configurationTag;
+    @Mock
+    private Xpp3Dom rulesTag;
+    @Mock
+    private Xpp3Dom requireMavenVersionTag;
+    @Mock
+    private Xpp3Dom versionTag;
+    @Mock
     private MavenProject parentMavenProject;
     @Mock
-    private Prerequisites prerequisites;
+    private Prerequisites parentPrerequisites;
 
     @Before
     public void setup() {
         when(mavenProject.getPrerequisites()).thenReturn(null);
         when(mavenProject.getBuildPlugins()).thenReturn(null);
         when(mavenProject.hasParent()).thenReturn(false);
-        when(mavenProject.getParent()).thenReturn(null);
         when(prerequisites.getMaven()).thenReturn(null);
+        when(nonEnforcerPlugin.getArtifactId()).thenReturn(null);
+        when(enforcerPlugin.getArtifactId()).thenReturn("maven-enforcer-plugin");
+        when(enforcerPlugin.getExecutions()).thenReturn(null);
+        when(pluginExecution.getGoals()).thenReturn(null);
+        when(parentMavenProject.getPrerequisites()).thenReturn(null);
+        when(parentMavenProject.getBuildPlugins()).thenReturn(null);
+        when(parentMavenProject.hasParent()).thenReturn(false);
+        when(parentPrerequisites.getMaven()).thenReturn(null);
     }
 
     @Test
-    public void findReturnsNullWhenPrerequisitesAndEnforcerPluginNotSet() {
+    public void findReturnsNullWhenPrerequisitesAreNullAndBuildPluginListIsNull() {
         assertNull(new RequiredMavenVersionFinder(mavenProject).find());
     }
 
-    // todo: find when prerequisites is not null and prerequisites.getMaven() is null
+    @Test
+    public void findReturnsNullWhenPrerequisitesMavenVersionIsNull() {
+        when(mavenProject.getPrerequisites()).thenReturn(prerequisites);
+        assertNull(new RequiredMavenVersionFinder(mavenProject).find());
+    }
 
-    // todo: find when prerequisites is not null and prerequisites.getMaven() is not null
+    @Test
+    public void findReturnsNotNullWhenPrerequisitesMavenVersionIsNotNull() {
+        String mavenVersion = "1";
+        DefaultArtifactVersion artifactVersion = new DefaultArtifactVersion(mavenVersion);
+        when(mavenProject.getPrerequisites()).thenReturn(prerequisites);
+        when(prerequisites.getMaven()).thenReturn(mavenVersion);
+        assertEquals(artifactVersion, new RequiredMavenVersionFinder(mavenProject).find());
+    }
 
-    // todo: find when buildPlugins is not null but does not contain enforcer plugin
+    @Test
+    public void findReturnsNullWhenBuildPluginsListDoesNotContainEnforcerPlugin() {
+        ArrayList<Plugin> buildPlugins = new ArrayList<>();
+        buildPlugins.add(nonEnforcerPlugin);
+        when(mavenProject.getBuildPlugins()).thenReturn(buildPlugins);
+        assertNull(new RequiredMavenVersionFinder(mavenProject).find());
+    }
 
-    // todo: find when mavenEnforcerPlugin.getExecutions() is null
+    @Test
+    public void findReturnsNullWhenEnforcerExecutionsIsNull() {
+        ArrayList<Plugin> buildPlugins = new ArrayList<>();
+        buildPlugins.add(enforcerPlugin);
+        when(mavenProject.getBuildPlugins()).thenReturn(buildPlugins);
+        assertNull(new RequiredMavenVersionFinder(mavenProject).find());
+    }
 
-    // todo: find when mavenEnforcerPlugin.getExecutions() is empty list
+    @Test
+    public void findReturnsNullWhenExecutionsListIsEmpty() {
+        ArrayList<Plugin> buildPlugins = new ArrayList<>();
+        buildPlugins.add(enforcerPlugin);
+        when(mavenProject.getBuildPlugins()).thenReturn(buildPlugins);
+        ArrayList<PluginExecution> pluginExecutions = new ArrayList<>();
+        when(enforcerPlugin.getExecutions()).thenReturn(pluginExecutions);
+        assertNull(new RequiredMavenVersionFinder(mavenProject).find());
+    }
 
-    // todo: find when mavenEnforcerPlugin.getExecutions() is not empty but does not contain enforce goal
+    @Test
+    public void findReturnsNullWhenExecutionGoalsListIsNull() {
+        ArrayList<Plugin> buildPlugins = new ArrayList<>();
+        buildPlugins.add(enforcerPlugin);
+        when(mavenProject.getBuildPlugins()).thenReturn(buildPlugins);
+        ArrayList<PluginExecution> pluginExecutions = new ArrayList<>();
+        pluginExecutions.add(pluginExecution);
+        when(enforcerPlugin.getExecutions()).thenReturn(pluginExecutions);
+        assertNull(new RequiredMavenVersionFinder(mavenProject).find());
+    }
 
-    // todo: find when pluginExecutionWithEnforceGoal.getConfiguration() is null
+    @Test
+    public void findReturnsNullWhenPopulatedExecutionsListDoNotContainEnforcerExecution() {
+        ArrayList<Plugin> buildPlugins = new ArrayList<>();
+        buildPlugins.add(enforcerPlugin);
+        when(mavenProject.getBuildPlugins()).thenReturn(buildPlugins);
+        ArrayList<PluginExecution> pluginExecutions = new ArrayList<>();
+        pluginExecutions.add(pluginExecution);
+        ArrayList<String> goals = new ArrayList<>();
+        when(pluginExecution.getGoals()).thenReturn(goals);
+        when(enforcerPlugin.getExecutions()).thenReturn(pluginExecutions);
+        assertNull(new RequiredMavenVersionFinder(mavenProject).find());
+    }
 
-    // todo: find when configurationTag.getChild("rules") is null
+    @Test
+    public void findReturnsNullWhenEnforceGoalConfigurationIsNull() {
+        ArrayList<Plugin> buildPlugins = new ArrayList<>();
+        buildPlugins.add(enforcerPlugin);
+        when(mavenProject.getBuildPlugins()).thenReturn(buildPlugins);
+        ArrayList<PluginExecution> pluginExecutions = new ArrayList<>();
+        pluginExecutions.add(pluginExecution);
+        ArrayList<String> goals = new ArrayList<>();
+        goals.add("enforce");
+        when(pluginExecution.getGoals()).thenReturn(goals);
+        when(enforcerPlugin.getExecutions()).thenReturn(pluginExecutions);
+        when(pluginExecution.getConfiguration()).thenReturn(null);
+        assertNull(new RequiredMavenVersionFinder(mavenProject).find());
+    }
 
-    // todo: find when rulesTag.getChild("requireMavenVersion") is null
+    @Test
+    public void findReturnsNullWhenRulesChildIsNull() {
+        ArrayList<Plugin> buildPlugins = new ArrayList<>();
+        buildPlugins.add(enforcerPlugin);
+        when(mavenProject.getBuildPlugins()).thenReturn(buildPlugins);
+        ArrayList<PluginExecution> pluginExecutions = new ArrayList<>();
+        pluginExecutions.add(pluginExecution);
+        ArrayList<String> goals = new ArrayList<>();
+        goals.add("enforce");
+        when(pluginExecution.getGoals()).thenReturn(goals);
+        when(enforcerPlugin.getExecutions()).thenReturn(pluginExecutions);
+        when(pluginExecution.getConfiguration()).thenReturn(configurationTag);
+        when(configurationTag.getChild("rules")).thenReturn(null);
+        assertNull(new RequiredMavenVersionFinder(mavenProject).find());
+    }
 
-    // todo: find when requireMavenVersionTag.getChild("version") is null
+    @Test
+    public void findReturnsNullWhenRequiredMavenVersionChildIsNull() {
+        ArrayList<Plugin> buildPlugins = new ArrayList<>();
+        buildPlugins.add(enforcerPlugin);
+        when(mavenProject.getBuildPlugins()).thenReturn(buildPlugins);
+        ArrayList<PluginExecution> pluginExecutions = new ArrayList<>();
+        pluginExecutions.add(pluginExecution);
+        ArrayList<String> goals = new ArrayList<>();
+        goals.add("enforce");
+        when(pluginExecution.getGoals()).thenReturn(goals);
+        when(enforcerPlugin.getExecutions()).thenReturn(pluginExecutions);
+        when(pluginExecution.getConfiguration()).thenReturn(configurationTag);
+        when(configurationTag.getChild("rules")).thenReturn(rulesTag);
+        when(rulesTag.getChild("requireMavenVersion")).thenReturn(null);
+        assertNull(new RequiredMavenVersionFinder(mavenProject).find());
+    }
 
-    // todo: find when versionTag.getValue() is null
+    @Test
+    public void findReturnsNullWhenVersionChildIsNull() {
+        ArrayList<Plugin> buildPlugins = new ArrayList<>();
+        buildPlugins.add(enforcerPlugin);
+        when(mavenProject.getBuildPlugins()).thenReturn(buildPlugins);
+        ArrayList<PluginExecution> pluginExecutions = new ArrayList<>();
+        pluginExecutions.add(pluginExecution);
+        ArrayList<String> goals = new ArrayList<>();
+        goals.add("enforce");
+        when(pluginExecution.getGoals()).thenReturn(goals);
+        when(enforcerPlugin.getExecutions()).thenReturn(pluginExecutions);
+        when(pluginExecution.getConfiguration()).thenReturn(configurationTag);
+        when(configurationTag.getChild("rules")).thenReturn(rulesTag);
+        when(rulesTag.getChild("requireMavenVersion")).thenReturn(requireMavenVersionTag);
+        when(requireMavenVersionTag.getChild("version")).thenReturn(null);
+        assertNull(new RequiredMavenVersionFinder(mavenProject).find());
+    }
 
-    // todo: find when child without version set and parent without set
+    @Test
+    public void findReturnsNullWhenVersionTagValueIsNull() {
+        ArrayList<Plugin> buildPlugins = new ArrayList<>();
+        buildPlugins.add(enforcerPlugin);
+        when(mavenProject.getBuildPlugins()).thenReturn(buildPlugins);
+        ArrayList<PluginExecution> pluginExecutions = new ArrayList<>();
+        pluginExecutions.add(pluginExecution);
+        ArrayList<String> goals = new ArrayList<>();
+        goals.add("enforce");
+        when(pluginExecution.getGoals()).thenReturn(goals);
+        when(enforcerPlugin.getExecutions()).thenReturn(pluginExecutions);
+        when(pluginExecution.getConfiguration()).thenReturn(configurationTag);
+        when(configurationTag.getChild("rules")).thenReturn(rulesTag);
+        when(rulesTag.getChild("requireMavenVersion")).thenReturn(requireMavenVersionTag);
+        when(requireMavenVersionTag.getChild("version")).thenReturn(versionTag);
+        when(versionTag.getValue()).thenReturn(null);
+        assertNull(new RequiredMavenVersionFinder(mavenProject).find());
+    }
 
-    // todo: find when child without version set and parent with version set
+    @Test
+    public void findReturnsValueWhenVersionTagValueIsNotNull() {
+        ArrayList<Plugin> buildPlugins = new ArrayList<>();
+        buildPlugins.add(enforcerPlugin);
+        when(mavenProject.getBuildPlugins()).thenReturn(buildPlugins);
+        ArrayList<PluginExecution> pluginExecutions = new ArrayList<>();
+        pluginExecutions.add(pluginExecution);
+        ArrayList<String> goals = new ArrayList<>();
+        goals.add("enforce");
+        when(pluginExecution.getGoals()).thenReturn(goals);
+        when(enforcerPlugin.getExecutions()).thenReturn(pluginExecutions);
+        when(pluginExecution.getConfiguration()).thenReturn(configurationTag);
+        when(configurationTag.getChild("rules")).thenReturn(rulesTag);
+        when(rulesTag.getChild("requireMavenVersion")).thenReturn(requireMavenVersionTag);
+        when(requireMavenVersionTag.getChild("version")).thenReturn(versionTag);
+        String mavenVersion = "1";
+        when(versionTag.getValue()).thenReturn(mavenVersion);
+        DefaultArtifactVersion artifactVersion = new DefaultArtifactVersion(mavenVersion);
+        assertEquals(artifactVersion, new RequiredMavenVersionFinder(mavenProject).find());
+    }
 
-    // todo: find when child with version set and parent without version set
+    @Test
+    public void findReturnsNullWhenChildWithoutVersionAndParentWithoutVersion() {
+        when(mavenProject.hasParent()).thenReturn(true);
+        when(mavenProject.getParent()).thenReturn(parentMavenProject);
+        assertNull(new RequiredMavenVersionFinder(mavenProject).find());
+    }
 
-    // todo: find when child and parent with equal version set
+    @Test
+    public void findReturnsChildVersionWhenChildWithVersionAndParentWithoutVersion() {
+        when(mavenProject.hasParent()).thenReturn(true);
+        when(mavenProject.getParent()).thenReturn(parentMavenProject);
+        String childMavenVersion = "1";
+        DefaultArtifactVersion childArtifactVersion = new DefaultArtifactVersion(childMavenVersion);
+        when(mavenProject.getPrerequisites()).thenReturn(prerequisites);
+        when(prerequisites.getMaven()).thenReturn(childMavenVersion);
+        assertEquals(childArtifactVersion, new RequiredMavenVersionFinder(mavenProject).find());
+    }
 
-    // todo: find when child has higher version than parent
+    @Test
+    public void findReturnsParentVersionWhenChildWithoutVersionAndParentWithVersion() {
+        when(mavenProject.hasParent()).thenReturn(true);
+        when(mavenProject.getParent()).thenReturn(parentMavenProject);
+        String parentMavenVersion = "1";
+        DefaultArtifactVersion parentArtifactVersion = new DefaultArtifactVersion(parentMavenVersion);
+        when(parentMavenProject.getPrerequisites()).thenReturn(parentPrerequisites);
+        when(parentPrerequisites.getMaven()).thenReturn(parentMavenVersion);
+        assertEquals(parentArtifactVersion, new RequiredMavenVersionFinder(mavenProject).find());
+    }
 
-    // todo: find when child has lower version than parent
+    @Test
+    public void findReturnsChildVersionWhenChildWithMatchingVersionAndParentWithMatchingVersion() {
+        when(mavenProject.hasParent()).thenReturn(true);
+        when(mavenProject.getParent()).thenReturn(parentMavenProject);
+        String childMavenVersion = "1";
+        DefaultArtifactVersion childArtifactVersion = new DefaultArtifactVersion(childMavenVersion);
+        when(mavenProject.getPrerequisites()).thenReturn(prerequisites);
+        when(prerequisites.getMaven()).thenReturn(childMavenVersion);
+        String parentMavenVersion = "1";
+        when(parentMavenProject.getPrerequisites()).thenReturn(parentPrerequisites);
+        when(parentPrerequisites.getMaven()).thenReturn(parentMavenVersion);
+        assertEquals(childArtifactVersion, new RequiredMavenVersionFinder(mavenProject).find());
+    }
+
+    @Test
+    public void findReturnsChildVersionWhenChildWithHigherVersionAndParentWithLowerVersion() {
+        when(mavenProject.hasParent()).thenReturn(true);
+        when(mavenProject.getParent()).thenReturn(parentMavenProject);
+        String childMavenVersion = "2";
+        DefaultArtifactVersion childArtifactVersion = new DefaultArtifactVersion(childMavenVersion);
+        when(mavenProject.getPrerequisites()).thenReturn(prerequisites);
+        when(prerequisites.getMaven()).thenReturn(childMavenVersion);
+        String parentMavenVersion = "1";
+        when(parentMavenProject.getPrerequisites()).thenReturn(parentPrerequisites);
+        when(parentPrerequisites.getMaven()).thenReturn(parentMavenVersion);
+        assertEquals(childArtifactVersion, new RequiredMavenVersionFinder(mavenProject).find());
+    }
+
+    @Test
+    public void findReturnsParentVersionWhenChildWithLowerVersionAndParentWithHigherVersion() {
+        when(mavenProject.hasParent()).thenReturn(true);
+        when(mavenProject.getParent()).thenReturn(parentMavenProject);
+        String childMavenVersion = "1";
+        when(mavenProject.getPrerequisites()).thenReturn(prerequisites);
+        when(prerequisites.getMaven()).thenReturn(childMavenVersion);
+        String parentMavenVersion = "2";
+        DefaultArtifactVersion parentArtifactVersion = new DefaultArtifactVersion(parentMavenVersion);
+        when(parentMavenProject.getPrerequisites()).thenReturn(parentPrerequisites);
+        when(parentPrerequisites.getMaven()).thenReturn(parentMavenVersion);
+        assertEquals(parentArtifactVersion, new RequiredMavenVersionFinder(mavenProject).find());
+    }
 }
