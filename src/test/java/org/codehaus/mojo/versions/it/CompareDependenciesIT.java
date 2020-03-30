6c8b52a6cddd89da9392ba2276a72c1c849b5644
Karl Heinz Marbaise <khmarbaise@apache.org>
2020-03-30 21:35:40 +0200

    First step.

---
+++ src/test/java/org/codehaus/mojo/versions/it/CompareDependenciesIT.java
@@ -0,0 +1,24 @@
+package org.codehaus.mojo.versions.it;
+
+import org.apache.maven.jupiter.extension.MavenIT;
+import org.apache.maven.jupiter.extension.MavenTest;
+import org.apache.maven.jupiter.maven.MavenExecutionResult;
+import org.apache.maven.jupiter.maven.MavenProjectResult;
+
+import static org.apache.maven.assertj.MavenITAssertions.assertThat;
+
+@MavenIT
+class CompareDependenciesIT {
+
+  @MavenTest(goals = {"${project.groupId}:${project.artifactId}:${project.version}:display-dependency-updates"},
+  systemProperties = {"remotePom=localhost:dummy-bom-pom:1.0", "reportMode=false",
+   "updatePropertyVersions=true"})
+  void it_compare_dependencies_001(MavenExecutionResult result, MavenProjectResult project) {
+    assertThat(result).isSuccessful();
+    assertThat(project).hasTarget()
+      .withEarFile()
+      .containsOnlyOnce("META-INF/application.xml", "META-INF/appserver-application.xml");
+  }
+
+
+}
