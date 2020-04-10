d1cb8204019526d78903e44e9dc53eb9562c69f5
Karl Heinz Marbaise <khmarbaise@apache.org>
2020-04-10 19:43:55 +0200

    Replaced replacement.

--- src/test/java/org/codehaus/mojo/versions/it/CompareDependenciesIT.java
+++ src/test/java/org/codehaus/mojo/versions/it/CompareDependenciesIT.java
@@ -9,7 +9,7 @@ import static org.apache.maven.assertj.MavenITAssertions.assertThat;
 @MavenIT
 class CompareDependenciesIT {
 
-  @MavenTest(goals = {"${project.groupId}:${project.artifactId}:${project.version}:compare-dependencies"},
+  @MavenTest(goals = {"org.codehaus.mojo:versions-maven-plugin:${project.version}:compare-dependencies"},
     systemProperties = {"remotePom=localhost:dummy-bom-pom:1.0", "reportOutputFile=target/depDiffs.txt"})
   void it_compare_dependencies_001(MavenExecutionResult result) {
     assertThat(result).isSuccessful();
