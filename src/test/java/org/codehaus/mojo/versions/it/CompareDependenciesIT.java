6a9bde62fd86e168d6fa9f71f4da2769656b95c5
Karl Heinz Marbaise <khmarbaise@apache.org>
2020-04-12 00:29:03 +0200

    Currently fail based on missing of MRM?

--- src/test/java/org/codehaus/mojo/versions/it/CompareDependenciesIT.java
+++ src/test/java/org/codehaus/mojo/versions/it/CompareDependenciesIT.java
@@ -19,8 +19,94 @@ class CompareDependenciesIT
                 .project()
                 .hasTarget()
                 .withFile( "depDiffs.txt" )
-                .hasContent( String.join( "\n", "The following differences were found:", "", "  none", "",
-                        "The following property differences were found:", "", "  none" ) );
+                .hasContent( String.join( "\n",
+                        "The following differences were found:",
+                    "",
+                    "  org.apache.maven:maven-artifact ..................... 2.0.10 -> 2.0.9",
+                    "",
+                    "The following property differences were found:",
+                    "",
+                    "  none" ) );
+    }
+
+    @MavenTest( goals = {"${project.groupId}:${project.artifactId}:${project.version}:compare-dependencies"},
+                systemProperties = {"remotePom=localhost:dummy-bom-pom:1.0", "reportMode=false", "updatePropertyVersions=true"} )
+    void it_compare_dependencies_002( MavenExecutionResult result, MavenProjectResult mavenProjectResult )
+    {
+        assertThat( result ).isSuccessful()
+                .project()
+                .hasTarget()
+                .withFile( "depDiffs.txt" )
+                .hasContent( String.join( "\n",
+                        "The following differences were found:",
+                    "",
+                    "  org.apache.maven:maven-artifact ..................... 2.0.10 -> 2.0.9",
+                    "",
+                    "The following property differences were found:",
+                    "",
+                    "  none" ) );
+    }
+    @MavenTest( goals = {"${project.groupId}:${project.artifactId}:${project.version}:compare-dependencies"},
+                systemProperties = {"remotePom=localhost:dummy-bom-maven-mismatch:1.0", "reportMode=false", "updatePropertyVersions=true"} )
+    void it_compare_dependencies_003( MavenExecutionResult result, MavenProjectResult mavenProjectResult )
+    {
+        assertThat( result ).isSuccessful()
+                .project()
+                .hasTarget()
+                .withFile( "depDiffs.txt" )
+                .hasContent( String.join( "\n",
+                        "The following differences were found:",
+                    "",
+                    "  org.apache.maven:maven-artifact ..................... 2.0.10 -> 2.0.9",
+                    "",
+                    "The following property differences were found:",
+                    "",
+                    "  none" ) );
+    }
+
+    @MavenTest( goals = {"${project.groupId}:${project.artifactId}:${project.version}:compare-dependencies"},
+                systemProperties = {
+            "remotePom=localhost:dummy-bom-pom:1.0",
+            "reportMode=true",
+            "reportOutputFile=target/depDiffs.txt",
+            "updatePropertyVersions=true"} )
+    void it_compare_dependencies_004( MavenExecutionResult result, MavenProjectResult mavenProjectResult )
+    {
+        assertThat( result ).isSuccessful()
+                .project()
+                .hasTarget()
+                .withFile( "depDiffs.txt" )
+                .hasContent( String.join( "\n",
+                        "The following differences were found:",
+                        "",
+                        "  org.apache.maven:maven-artifact ..................... 2.0.10 -> 2.0.9",
+                        "  junit:junit .............................................. 4.8 -> 4.1",
+                        "",
+                        "The following property differences were found:",
+                        "",
+                        "  junit.version ............................................ 4.8 -> 4.1" ) );
+    }
+
+    @MavenTest( goals = {"${project.groupId}:${project.artifactId}:${project.version}:compare-dependencies"},
+                systemProperties = {
+            "remotePom=localhost:dummy-bom-pom:1.0",
+            "reportMode=true",
+            "reportOutputFile=target/depDiffs.txt",
+            "updatePropertyVersions=true"} )
+    void it_compare_dependencies_005( MavenExecutionResult result, MavenProjectResult mavenProjectResult )
+    {
+        assertThat( result ).isSuccessful()
+                .project()
+                .hasTarget()
+                .withFile( "depDiffs.txt" )
+                .hasContent( String.join( "\n",
+        "The following differences were found:",
+                    "",
+                    "  org.apache.maven:maven-artifact ..................... 2.0.10 -> 2.0.9",
+                    "",
+                    "The following property differences were found:",
+                    "",
+                    "  none" ));
     }
 
 
