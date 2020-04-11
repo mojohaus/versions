f463b268d465fe4ef170ece0f04a45bf5050f1e8
Karl Heinz Marbaise <khmarbaise@apache.org>
2020-04-11 22:15:13 +0200

    Using full placeholders.

--- src/test/java/org/codehaus/mojo/versions/it/CompareDependenciesIT.java
+++ src/test/java/org/codehaus/mojo/versions/it/CompareDependenciesIT.java
@@ -3,15 +3,16 @@ package org.codehaus.mojo.versions.it;
 import org.apache.maven.jupiter.extension.MavenIT;
 import org.apache.maven.jupiter.extension.MavenTest;
 import org.apache.maven.jupiter.maven.MavenExecutionResult;
+import org.apache.maven.jupiter.maven.MavenProjectResult;
 
 import static org.apache.maven.assertj.MavenITAssertions.assertThat;
 
 @MavenIT
 class CompareDependenciesIT {
 
-  @MavenTest(goals = {"org.codehaus.mojo:versions-maven-plugin:${project.version}:compare-dependencies"},
+  @MavenTest(goals = {"${project.groupId}:${project.artifactId}:${project.version}:compare-dependencies"},
     systemProperties = {"remotePom=localhost:dummy-bom-pom:1.0", "reportOutputFile=target/depDiffs.txt"})
-  void it_compare_dependencies_001(MavenExecutionResult result) {
+  void it_compare_dependencies_001(MavenExecutionResult result, MavenProjectResult mavenProjectResult ) {
     assertThat(result).isSuccessful();
 //    assertThat(project).hasTarget()
 //      .withEarFile()
