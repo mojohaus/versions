994e03bb6bd32b6a44b47c2f92ff58bee880ace2
Karl Heinz Marbaise <khmarbaise@apache.org>
2020-04-13 15:41:59 +0200

    Enhanced IT

--- src/test/java/org/codehaus/mojo/versions/it/SetIT.java
+++ src/test/java/org/codehaus/mojo/versions/it/SetIT.java
@@ -9,6 +9,7 @@ import org.apache.maven.jupiter.maven.MavenProjectResult;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.TestMethodOrder;
 
 import static org.apache.maven.assertj.MavenITAssertions.assertThat;
@@ -28,17 +29,33 @@ class SetIT
 
         @MavenTest( options = MavenOptions.NON_RECURSIVE, goals = {VERSIONS_PLUGIN + ":set"},
                     systemProperties = {"newVersion=2.0"} )
-        void first( MavenExecutionResult result, MavenProjectResult mavenProjectResult )
+        @Order( 10 )
+        void first( MavenExecutionResult result )
         {
             assertThat( result ).isSuccessful();
         }
 
         @MavenTest( options = MavenOptions.NON_RECURSIVE, goals = {VERSIONS_PLUGIN + ":set"},
                     systemProperties = {"newVersion=2.0", "groupId=*", "artifactId=*", "oldVersion=*"} )
-        void second( MavenExecutionResult result, MavenProjectResult mavenProjectResult )
+        @Order( 20 )
+        void second( MavenExecutionResult result)
         {
             assertThat( result ).isSuccessful();
         }
     }
 
+    @MavenTest( goals = {VERSIONS_PLUGIN + ":set"},
+                systemProperties = {"newVersion=2.3"} )
+    void set_001_issue_76 (MavenExecutionResult result) {
+        assertThat( result ).isSuccessful()
+            .project().hasModule( "xx" ).hasPom().
+              .hasPom().withVersion("2.3")
+                       .hasDependencies().with;
+
+        /*
+        invoker.goals=${project.groupId}:${project.artifactId}:${project.version}:set -DnewVersion=2.3
+invoker.buildResult=success
+
+         */
+    }
 }
