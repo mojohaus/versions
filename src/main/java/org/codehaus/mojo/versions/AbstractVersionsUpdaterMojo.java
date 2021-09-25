d3af57e3ec9f41405d54216684db0272c32c1817
violette <violette.roche-montane@forgerock.com>
2021-09-25 14:10:29 +0200

    fix typo

--- src/main/java/org/codehaus/mojo/versions/AbstractVersionsUpdaterMojo.java
+++ src/main/java/org/codehaus/mojo/versions/AbstractVersionsUpdaterMojo.java
@@ -531,7 +531,7 @@ public abstract class AbstractVersionsUpdaterMojo
             getLog().info( "Property ${" + property.getName() + "}: Leaving unchanged as " + currentVersion );
         }
         else if ( PomHelper.setPropertyVersion( pom, version.getProfileId(), property.getName(), winner.toString() ) ) {
-            String updateMessage = "Updated ${" + property.getName() + "} from " + currentVersion + " to " + winner;
+            String updateMessage = "Update ${" + property.getName() + "} from " + currentVersion + " to " + winner;
             lock.lock();
             try {
                 // To have separated commits we need to write the pom each time a property is updated
