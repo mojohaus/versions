7b16743767756b151230a25b706d3f3979578225
Stefan Seifert <stefanseifert@users.noreply.github.com>
2022-01-18 10:27:25 +0100

    Site checkin for project Versions Maven Plugin

---
+++ testapidocs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"org.codehaus.mojo.versions"},{"l":"org.codehaus.mojo.versions.api"},{"l":"org.codehaus.mojo.versions.ordering"},{"l":"org.codehaus.mojo.versions.utils"}]
