3faee38d5b1aec68408791ea1fc32dc9dc923c96
Slawomir Jaranowski <s.jaranowski@gmail.com>
2022-09-06 08:50:04 +0200

    Site checkin for project Versions Maven Plugin

--- testapidocs/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"org.codehaus.mojo.versions"},{"l":"org.codehaus.mojo.versions.api"},{"l":"org.codehaus.mojo.versions.ordering"},{"l":"org.codehaus.mojo.versions.recording"},{"l":"org.codehaus.mojo.versions.utils"}]
