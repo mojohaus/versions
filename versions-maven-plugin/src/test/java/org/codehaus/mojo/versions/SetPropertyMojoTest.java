19d03f352eb528302ab69b3fb79399f850d47e4d
Marco Carletti <mcarlett@redhat.com>
2023-01-26 22:02:44 +0100

    Fixed #911- add profile parameter in set-property
    Add profileId property to manage the property value to set

--- versions-maven-plugin/src/test/java/org/codehaus/mojo/versions/SetPropertyMojoTest.java
+++ versions-maven-plugin/src/test/java/org/codehaus/mojo/versions/SetPropertyMojoTest.java
@@ -22,10 +22,14 @@ package org.codehaus.mojo.versions;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.NoSuchElementException;
+import java.util.UUID;
 
+import org.apache.maven.model.Model;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.testing.AbstractMojoTestCase;
 import org.apache.maven.plugin.testing.MojoRule;
+import org.codehaus.mojo.versions.api.PomHelper;
 import org.eclipse.aether.resolution.VersionRangeRequest;
 import org.eclipse.aether.resolution.VersionRangeResult;
 import org.junit.After;
@@ -38,6 +42,7 @@ import static org.codehaus.mojo.versions.utils.TestUtils.createTempDir;
 import static org.codehaus.mojo.versions.utils.TestUtils.tearDownTempDir;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.matchesPattern;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
@@ -120,5 +125,79 @@ public class SetPropertyMojoTest extends AbstractMojoTestCase {
                     e.getMessage(),
                     containsString("Please provide either 'property' or 'propertiesVersionsFile' parameter."));
         }
+    }
+
+    @Test
+    public void testChangeOnlyPropertiesInTheProfile() throws Exception {
+        final String newVersion = UUID.randomUUID().toString();
+        final Model model = getModelForProfile("test-profile", true, newVersion);
+
+        assertThat(model.getProperties().getProperty("dummy-api-version"), is("1.0.0"));
+        assertThat(
+                model.getProfiles().stream()
+                        .filter(profile -> "test-profile".equals(profile.getId()))
+                        .findFirst()
+                        .orElseThrow(() -> new NoSuchElementException("profile test-profile not found"))
+                        .getProperties()
+                        .getProperty("dummy-api-version"),
+                is(newVersion));
+    }
+
+    @Test
+    public void testKeepPropertiesInTheProfile() throws Exception {
+        final String newVersion = UUID.randomUUID().toString();
+        final Model model = getModelForProfile("test-profile", false, newVersion);
+
+        assertThat(model.getProperties().getProperty("dummy-api-version"), is(newVersion));
+        assertThat(
+                model.getProfiles().stream()
+                        .filter(profile -> "test-profile".equals(profile.getId()))
+                        .findFirst()
+                        .orElseThrow(() -> new NoSuchElementException("profile test-profile not found"))
+                        .getProperties()
+                        .getProperty("dummy-api-version"),
+                is("test-value"));
+    }
+
+    @Test
+    public void testDoNotChangePropertyIfTheProfileNotfound() throws Exception {
+        final Model model =
+                getModelForProfile("new-profile", true, UUID.randomUUID().toString());
+
+        assertThat(model.getProperties().getProperty("dummy-api-version"), is("1.0.0"));
+        assertThat(
+                model.getProfiles().stream()
+                        .filter(profile -> "new-profile".equals(profile.getId()))
+                        .count(),
+                is(0L));
+        assertThat(
+                model.getProfiles().stream()
+                        .filter(profile -> "test-profile".equals(profile.getId()))
+                        .findFirst()
+                        .orElseThrow(() -> new NoSuchElementException("profile test-profile not found"))
+                        .getProperties()
+                        .getProperty("dummy-api-version"),
+                is("test-value"));
+    }
+
+    private Model getModelForProfile(String profileName, Boolean setProfile, String newVersion) throws Exception {
+        copyDir(Paths.get("src/test/resources/org/codehaus/mojo/set-property/profiled-new-version"), pomDir);
+        SetPropertyMojo mojo = (SetPropertyMojo) mojoRule.lookupConfiguredMojo(pomDir.toFile(), "set-property");
+
+        mojo.aetherRepositorySystem = mock(org.eclipse.aether.RepositorySystem.class);
+        when(mojo.aetherRepositorySystem.resolveVersionRange(any(), any(VersionRangeRequest.class)))
+                .then(i -> new VersionRangeResult(i.getArgument(1)));
+
+        setVariableValueToObject(mojo, "newVersion", newVersion);
+
+        if (setProfile) {
+            setVariableValueToObject(mojo, "profileId", profileName);
+        }
+
+        mojo.execute();
+
+        final Model model = PomHelper.getRawModel(
+                Paths.get(pomDir.toAbsolutePath().toString(), "pom.xml").toFile());
+        return model;
     }
 }
