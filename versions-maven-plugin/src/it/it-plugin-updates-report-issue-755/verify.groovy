bb3e460fa15e7a07325ccdbb8716c4fd8ea640d5
Slawomir Jaranowski <s.jaranowski@gmail.com>
2023-07-24 20:38:16 +0200

    Use versions of dummy artifacts in IT assertions
    We can not use version of production artifact
    it can change between build

--- versions-maven-plugin/src/it/it-plugin-updates-report-issue-755/verify.groovy
+++ versions-maven-plugin/src/it/it-plugin-updates-report-issue-755/verify.groovy
@@ -1,5 +1,5 @@
 report = new File( basedir, "target/plugin-updates-report.xml" ).text.replaceAll( '\\s*', '' )
 
-assert report.contains( '<major>3.0.0</major>' )
-assert report.contains( '<minor>2.3</minor>' )
-assert report.contains( '<incremental>2.2.2</incremental>' )
+
+assert report.contains( '<minor>2.1</minor>' )
+assert report.contains( '<major>3.1</major>' )
