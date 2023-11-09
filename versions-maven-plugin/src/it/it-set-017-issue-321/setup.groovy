2fd9c32b3bfb618dd8835fa148ee832cf1da22e5
Slawomir Jaranowski <s.jaranowski@gmail.com>
2023-11-10 00:57:36 +0100

    Reading project model should throw exception
    In other case we can omit a module from list to process
    
    In Maven 4 model reading is more strict,
    when we have a bad file in project - ITs can not start because of strict parsing

---
+++ versions-maven-plugin/src/it/it-set-017-issue-321/setup.groovy
@@ -0,0 +1,23 @@
+import java.nio.file.Files
+/*
+ * Copyright MojoHaus and Contributors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+// rename file before run test
+def module2Path = basedir.toPath().resolve('module-a2')
+Files.move(module2Path.resolve('pom.invalid'), module2Path.resolve('pom.xml'))
+
+return true
