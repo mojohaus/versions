9c72d8768f36bbee02a7d795eb4b32f54720de44
Olivier Lamy <olamy@apache.org>
2023-11-17 10:07:55 +1000

    Site checkin for project Versions

--- versions-api/apidocs/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"org.codehaus.mojo.versions.api.change"},{"l":"org.codehaus.mojo.versions.api.recording"}];updateSearchResults();
