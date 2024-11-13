5f9c29e61bcd2348c4e6f8ca4dd4eca43436eb11
Slawomir Jaranowski <s.jaranowski@gmail.com>
2024-11-13 21:47:20 +0100

    Site checkin for project Versions

---
+++ versions-model-report/apidocs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"org.codehaus.mojo.versions.reporting.model"},{"l":"org.codehaus.mojo.versions.reporting.model.io.stax"}];updateSearchResults();
---
+++ versions-model/apidocs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"org.codehaus.mojo.versions.model","l":"IgnoreVersion"},{"p":"org.codehaus.mojo.versions.model","l":"Rule"},{"p":"org.codehaus.mojo.versions.model","l":"RuleSet"},{"p":"org.codehaus.mojo.versions.model.io.stax","l":"RuleStaxReader"}];updateSearchResults();
