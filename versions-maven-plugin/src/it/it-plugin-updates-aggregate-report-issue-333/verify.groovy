7cf891706bb170a56110984d8cad2568ecb44e76
Andrzej J. <gh@jarmoniuk.nl>
2024-11-30 13:35:57 +0100

    Fixing the failed release build

--- versions-maven-plugin/src/it/it-plugin-updates-aggregate-report-issue-333/verify.groovy
+++ versions-maven-plugin/src/it/it-plugin-updates-aggregate-report-issue-333/verify.groovy
@@ -1,13 +1,13 @@
-output = new File( basedir, 'target/site/plugin-updates-aggregate-report.html' ).text
+def output = new File( basedir, 'target/reports/plugin-updates-aggregate-report.html' ).text
         .replaceAll( '<[^>]+>', ' ' )
         .replaceAll( '&[^;]+;', ' ' )
         .replaceAll( '\\s+', ' ' )
 
 // Plugin management
-assert !( output =~ 'This project does not declare any plugins in a build/pluginManagement section.' )
-assert ( output =~ 'org.apache.maven.plugins maven-clean-plugin 3.0.0' )
+assert !(output =~ 'This project does not declare any plugins in a build/pluginManagement section.')
+assert output =~ 'localhost dummy-maven-plugin 1.0'
 
 // Plugins
-assert !( output =~ 'This project does not declare any plugins in a build/plugins section.' )
-assert ( output =~ 'org.apache.maven.plugins maven-clean-plugin 3.1.0' )
-assert ( output =~ 'org.apache.maven.plugins maven-clean-plugin 3.2.0' )
+assert !(output =~ 'This project does not declare any plugins in a build/plugins section.')
+assert output =~ 'localhost dummy-maven-plugin 2.0'
+assert output =~ 'localhost dummy-maven-plugin 3.0'
