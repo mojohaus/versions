4fda959eaa47ddc936eeae139c26f9fc924b3feb
Schutte, M.A.R.O. (Marcel) <m.schutte@ohra.nl>
2025-10-09 16:36:47 +0200

    Fixed #1287 - Versionless dependencies in dependencyManagement accepted by maven, but not bij resolve-ranges
    Only use mock dependencies in integration tests

--- versions-maven-plugin/src/it/it-resolve-ranges-issue-1287/verify.groovy
+++ versions-maven-plugin/src/it/it-resolve-ranges-issue-1287/verify.groovy
@@ -1,2 +1,2 @@
 def buildLogFile = new File(basedir, "build.log")
-assert buildLogFile.text.contains("Found managed dependency junit:junit:jar without a version")
+assert buildLogFile.text.contains("Found managed dependency test.package:moduleTest:jar without a version")
