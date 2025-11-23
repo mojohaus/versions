6d6ab174a93f4b2265c7366a1890590d996c759f
Sylwester Lachiewicz <slachiewicz@apache.org>
2025-11-23 18:03:57 +0100

    Site checkin for project Versions

--- versions-enforcer/apidocs/type-search-index.js
+++ versions-enforcer/apidocs/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"org.codehaus.mojo.versions.enforcer","l":"MaxDependencyUpdates"}];updateSearchResults();
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html","k":"18"},{"p":"org.codehaus.mojo.versions.enforcer","l":"MaxDependencyUpdates"}];updateSearchResults();
--- versions-model/apidocs/package-search-index.js
+++ versions-model/apidocs/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"org.codehaus.mojo.versions.model"},{"l":"org.codehaus.mojo.versions.model.io.stax"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html","k":"18"},{"l":"org.codehaus.mojo.versions.model"},{"l":"org.codehaus.mojo.versions.model.io.stax"}];updateSearchResults();
