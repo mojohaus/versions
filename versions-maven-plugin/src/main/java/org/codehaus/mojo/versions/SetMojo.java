8610975b444ff038877a1bfafa19e2416e18d57d
MohammedGhallab <mohammedfahmiyghallab@gmail.com>
2026-04-12 06:25:52 +0300

    fix(set): ensure parent POM is updated when goal is executed from a child module

--- versions-maven-plugin/src/main/java/org/codehaus/mojo/versions/SetMojo.java
+++ versions-maven-plugin/src/main/java/org/codehaus/mojo/versions/SetMojo.java
@@ -407,7 +407,12 @@ public class SetMojo extends AbstractVersionsUpdaterMojo {
 
     private void execute(MavenProject currentProject) throws MojoExecutionException {
         if (currentProject.getOriginalModel().getVersion() == null && currentProject.getParent() == null) {
-            throw new MojoExecutionException("Project version is inherited from parent.");
+
+            if (!processAllModules && groupId != null) {
+                getLog().warn("Project version is inherited from parent, but proceeding to update references.");
+            } else {
+                throw new MojoExecutionException("Project version is inherited from parent.");
+            }
         }
 
         if (removeSnapshot && !nextSnapshot) {
@@ -478,10 +483,16 @@ public class SetMojo extends AbstractVersionsUpdaterMojo {
             // so that the main project can be selected
             Model rootModel = reactorModels.get(project.getFile());
 
-            if (project.getFile() != null) {
-                files.add(project.getFile());
-            }
+            if (project.getFile() != null && (session.getProjects().size() > 1 || project.equals(currentProject))) {
 
+                String rGroupId = PomHelper.getGroupId(rootModel);
+                String rArtifactId = rootModel.getArtifactId();
+
+                if (processAllModules ||
+                        (Objects.equals(rGroupId, groupId) && Objects.equals(rArtifactId, artifactId))) {
+                    files.add(project.getFile());
+                }
+            }
             String groupId = this.groupId;
             String artifactId = this.artifactId;
             String oldVersion = this.oldVersion;
@@ -561,8 +572,8 @@ public class SetMojo extends AbstractVersionsUpdaterMojo {
      */
     protected String getIncrementedVersion(String version, Integer nextSnapshotIndexToIncrement)
             throws MojoExecutionException {
-        String versionWithoutSnapshot =
-                version.endsWith(SNAPSHOT) ? version.substring(0, version.indexOf(SNAPSHOT)) : version;
+        String versionWithoutSnapshot = version.endsWith(SNAPSHOT) ? version.substring(0, version.indexOf(SNAPSHOT))
+                : version;
         String[] versionComponents = versionWithoutSnapshot.split("\\.");
 
         if (nextSnapshotIndexToIncrement == null) {
@@ -576,15 +587,15 @@ public class SetMojo extends AbstractVersionsUpdaterMojo {
 
         int finalNextSnapshotIndexToIncrement = nextSnapshotIndexToIncrement;
         return IntStream.range(0, versionComponents.length)
-                        .mapToObj(i -> {
-                            if (i + 1 < finalNextSnapshotIndexToIncrement) {
-                                return versionComponents[i];
-                            } else if (i + 1 == finalNextSnapshotIndexToIncrement) {
-                                return String.valueOf(Integer.parseInt(versionComponents[i]) + 1);
-                            }
-                            return "0";
-                        })
-                        .collect(Collectors.joining("."))
+                .mapToObj(i -> {
+                    if (i + 1 < finalNextSnapshotIndexToIncrement) {
+                        return versionComponents[i];
+                    } else if (i + 1 == finalNextSnapshotIndexToIncrement) {
+                        return String.valueOf(Integer.parseInt(versionComponents[i]) + 1);
+                    }
+                    return "0";
+                })
+                .collect(Collectors.joining("."))
                 + "-SNAPSHOT";
     }
 
@@ -609,9 +620,9 @@ public class SetMojo extends AbstractVersionsUpdaterMojo {
             final Model sourceModel = sourceEntry.getValue();
 
             getLog().debug(
-                            sourcePath.length() == 0
-                                    ? "Processing root module as parent"
-                                    : "Processing " + sourcePath + " as a parent.");
+                    sourcePath.length() == 0
+                            ? "Processing root module as parent"
+                            : "Processing " + sourcePath + " as a parent.");
 
             final String sourceGroupId = PomHelper.getGroupId(sourceModel);
             if (sourceGroupId == null) {
@@ -698,8 +709,7 @@ public class SetMojo extends AbstractVersionsUpdaterMojo {
             throws MojoExecutionException, MojoFailureException, XMLStreamException {
         ContextualLog log = new DelegatingContextualLog(getLog());
         try {
-            Model model =
-                    PomHelper.getRawModel(pom.getSource(), pom.getFileName().toFile());
+            Model model = PomHelper.getRawModel(pom.getSource(), pom.getFileName().toFile());
             log.setContext("Processing " + PomHelper.getGroupId(model) + ":" + PomHelper.getArtifactId(model));
 
             VersionChangerFactory versionChangerFactory = new VersionChangerFactory();
