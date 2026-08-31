mojohaus/versions

commit 74624c5d8c435a8f07c24cf1cb832a134ab144f1

Refactoring: coalescing use-*-* goals. (#1202)

versions-maven-plugin/src/main/java/org/codehaus/mojo/versions/UseLatestVersionsMojoBase.java
@@ -17,26 +17,34 @@ package org.codehaus.mojo.versions;
 
 import javax.xml.stream.XMLStreamException;
 
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Map;
 import java.util.Optional;
-import java.util.function.BiFunction;
-import java.util.function.Predicate;
+import java.util.stream.Stream;
 
 import org.apache.maven.artifact.Artifact;
 import org.apache.maven.artifact.handler.manager.ArtifactHandlerManager;
 import org.apache.maven.artifact.versioning.ArtifactVersion;
 import org.apache.maven.model.Dependency;
+import org.apache.maven.model.DependencyManagement;
 import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.wagon.Wagon;
 import org.codehaus.mojo.versions.api.ArtifactVersions;
+import org.codehaus.mojo.versions.api.PomHelper;
+import org.codehaus.mojo.versions.api.Segment;
 import org.codehaus.mojo.versions.api.VersionRetrievalException;
 import org.codehaus.mojo.versions.api.recording.ChangeRecorder;
 import org.codehaus.mojo.versions.api.recording.DependencyChangeRecord;
+import org.codehaus.mojo.versions.ordering.InvalidSegmentException;
 import org.codehaus.mojo.versions.rewriting.MutableXMLStreamReader;
 import org.codehaus.mojo.versions.utils.DefaultArtifactVersionCache;
+import org.codehaus.mojo.versions.utils.SegmentUtils;
 import org.eclipse.aether.RepositorySystem;
+
+import static java.util.Collections.singletonList;
 
 /**
  * Common base class for {@link UseLatestVersionsMojo}
@@ -51,58 +59,102 @@ public abstract class UseLatestVersionsMojoBase extends AbstractVersionsDependen
         super(artifactHandlerManager, repositorySystem, wagonMap, changeRecorders);
     }
 
+    protected abstract boolean isAllowMajorUpdates();
+
+    protected abstract boolean isAllowMinorUpdates();
+
+    protected abstract boolean isAllowIncrementalUpdates();
+
+    protected abstract boolean isAllowDowngrade();
+
+    protected abstract boolean updateFilter(Dependency dep);
+
+    protected abstract boolean artifactVersionsFilter(ArtifactVersion ver);
+
+    protected abstract Optional<ArtifactVersion> versionProducer(Stream<ArtifactVersion> stream);
+
     /**
-     * Updates the pom, given a set of dependencies, a function retrieving the newest version,
-     * and an optional array of filters against which the input dependencies are matched.
-     *
-     * @param pom POM to be modified
-     * @param dependencies collection of dependencies with the dependency versions before the change
-     * @param newestVersionProducer function providing the newest version given a dependency and
-     *                              an {@link ArtifactVersions} instance
-     * @param changeKind title for the change recorder records
-     * @param filters optional array of filters
-     * @throws XMLStreamException thrown if the POM update doesn't succeed
-     * @throws MojoExecutionException if something goes wrong.
-     * @throws VersionRetrievalException thrown if an artifact versions cannot be retrieved
+     * @param pom the pom to update.
+     * @throws org.apache.maven.plugin.MojoExecutionException when things go wrong
+     * @throws org.apache.maven.plugin.MojoFailureException   when things go wrong in a very bad way
+     * @throws javax.xml.stream.XMLStreamException            when things go wrong with XML streaming
+     * @see org.codehaus.mojo.versions.AbstractVersionsUpdaterMojo#update(MutableXMLStreamReader)
      */
-    @SafeVarargs
-    protected final void useLatestVersions(
+    protected void update(MutableXMLStreamReader pom)
+            throws MojoExecutionException, MojoFailureException, XMLStreamException, VersionRetrievalException {
+        if (isAllowDowngrade() && isAllowSnapshots()) {
+            throw new MojoExecutionException("allowDowngrade is only valid with allowSnapshots equal to false");
+        }
+
+        Optional<Segment> unchangedSegment = SegmentUtils.determineUnchangedSegment(
+                isAllowMajorUpdates(), isAllowMinorUpdates(), isAllowIncrementalUpdates(), getLog());
+        try {
+            if (isProcessingDependencyManagement()) {
+                DependencyManagement dependencyManagement =
+                        PomHelper.getRawModel(getProject()).getDependencyManagement();
+                if (dependencyManagement != null) {
+                    update(
+                            pom,
+                            dependencyManagement.getDependencies(),
+                            DependencyChangeRecord.ChangeKind.DEPENDENCY_MANAGEMENT,
+                            unchangedSegment);
+                }
+            }
+            if (getProject().getDependencies() != null && isProcessingDependencies()) {
+                update(
+                        pom,
+                        getProject().getDependencies(),
+                        DependencyChangeRecord.ChangeKind.DEPENDENCY,
+                        unchangedSegment);
+            }
+            if (getProject().getParent() != null && isProcessingParent()) {
+                update(
+                        pom,
+                        singletonList(getParentDependency()),
+                        DependencyChangeRecord.ChangeKind.PARENT,
+                        unchangedSegment);
+            }
+        } catch (IOException e) {
+            throw new MojoExecutionException(e.getMessage(), e);
+        }
+    }
+
+    protected final void update(
             MutableXMLStreamReader pom,
             Collection<Dependency> dependencies,
-            BiFunction<Dependency, ArtifactVersions, Optional<ArtifactVersion>> newestVersionProducer,
             DependencyChangeRecord.ChangeKind changeKind,
-            Predicate<Dependency>... filters)
+            Optional<Segment> unchangedSegment)
             throws XMLStreamException, MojoExecutionException, VersionRetrievalException {
         for (Dependency dep : dependencies) {
-            if (!Arrays.stream(filters)
-                    .map(f -> f.test(dep))
-                    .reduce(Boolean::logicalAnd)
-                    .orElse(true)) {
+            if (!updateFilter(dep)) {
                 continue;
-            }
-
-            if (isExcludeReactor() && isProducedByReactor(dep)) {
+            } else if (isExcludeReactor() && isProducedByReactor(dep)) {
                 getLog().info("Ignoring reactor dependency: " + toString(dep));
                 continue;
-            }
-
-            if (isHandledByProperty(dep)) {
+            } else if (isHandledByProperty(dep)) {
                 getLog().debug("Ignoring dependency with property as version: " + toString(dep));
                 continue;
             }
-
             Artifact artifact = toArtifact(dep);
             if (!isIncluded(artifact)) {
                 continue;
             }
+            if (getLog().isDebugEnabled()) {
+                ArtifactVersion selectedVersion = DefaultArtifactVersionCache.of(dep.getVersion());
+                getLog().debug("Selected version:" + selectedVersion);
+                getLog().debug("Looking for newer versions of " + toString(dep));
+            }
 
-            ArtifactVersion selectedVersion = DefaultArtifactVersionCache.of(dep.getVersion());
-            getLog().debug("Selected version:" + selectedVersion);
-            getLog().debug("Looking for newer versions of " + toString(dep));
             ArtifactVersions versions = getHelper().lookupArtifactVersions(artifact, false);
-            Optional<ArtifactVersion> newestVer = newestVersionProducer.apply(dep, versions);
-            if (newestVer.isPresent()) {
-                updateDependencyVersion(pom, dep, newestVer.get().toString(), changeKind);
+            try {
+                Optional<ArtifactVersion> newestVer = versionProducer(Arrays.stream(versions.getNewerVersions(
+                                dep.getVersion(), unchangedSegment, isAllowSnapshots(), isAllowDowngrade()))
+                        .filter(this::artifactVersionsFilter));
+                if (newestVer.isPresent()) {
+                    updateDependencyVersion(pom, dep, newestVer.get().toString(), changeKind);
+                }
+            } catch (InvalidSegmentException e) {
+                getLog().warn("Ignoring " + this.toString(dep) + " as the version number is too short");
             }
         }
     }

versions-maven-plugin/src/test/java/org/codehaus/mojo/versions/UseLatestReleasesMojoTest.java
@@ -19,21 +19,16 @@ import javax.xml.stream.XMLStreamException;
 
 import java.util.HashMap;
 
-import org.apache.maven.artifact.handler.manager.ArtifactHandlerManager;
 import org.apache.maven.model.Model;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.project.MavenProject;
-import org.codehaus.mojo.versions.api.PomHelper;
 import org.codehaus.mojo.versions.api.VersionRetrievalException;
 import org.codehaus.mojo.versions.change.DefaultDependencyVersionChange;
 import org.codehaus.mojo.versions.utils.DependencyBuilder;
-import org.codehaus.mojo.versions.utils.TestChangeRecorder;
-import org.eclipse.aether.RepositorySystem;
 import org.hamcrest.Matchers;
 import org.junit.Before;
 import org.junit.Test;
-import org.mockito.MockedStatic;
 
 import static java.util.Collections.emptyList;
 import static java.util.Collections.singletonList;
@@ -45,53 +40,42 @@ import static org.codehaus.mojo.versions.utils.MockUtils.mockMavenSession;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.empty;
 import static org.hamcrest.Matchers.hasItem;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.mockStatic;
 
-public class UseLatestReleasesMojoTest {
-    private UseLatestReleasesMojo mojo;
-    private TestChangeRecorder changeRecorder;
+public class UseLatestReleasesMojoTest extends UseLatestVersionsMojoTestBase {
 
     @Before
     public void setUp() throws Exception {
-        ArtifactHandlerManager artifactHandlerManager = mockArtifactHandlerManager();
-        RepositorySystem repositorySystem = mockAetherRepositorySystem(new HashMap<String, String[]>() {
-            {
-                put("dependency-artifact", new String[] {"0.9.0", "1.0.0-beta"});
-            }
-        });
-
-        changeRecorder = new TestChangeRecorder();
-
-        mojo = new UseLatestReleasesMojo(artifactHandlerManager, repositorySystem, null, changeRecorder.asTestMap()) {
-            {
-                reactorProjects = emptyList();
-                MavenProject project = new MavenProject() {
+        changeRecorder = createChangeRecorder();
+        mojo =
+                new UseLatestReleasesMojo(
+                        mockArtifactHandlerManager(), createRepositorySystem(), null, changeRecorder.asTestMap()) {
                     {
-                        setModel(new Model() {
+                        reactorProjects = emptyList();
+                        MavenProject project = new MavenProject() {
                             {
-                                setGroupId("default-group");
-                                setArtifactId("project-artifact");
-                                setVersion("1.0.0-SNAPSHOT");
+                                setModel(new Model() {
+                                    {
+                                        setGroupId("default-group");
+                                        setArtifactId("project-artifact");
+                                        setVersion("1.0.0-SNAPSHOT");
 
-                                setDependencies(singletonList(DependencyBuilder.newBuilder()
-                                        .withGroupId("default-group")
-                                        .withArtifactId("dependency-artifact")
-                                        .withVersion("0.9.0")
-                                        .withScope(SCOPE_COMPILE)
-                                        .withType("jar")
-                                        .withClassifier("default")
-                                        .build()));
+                                        setDependencies(singletonList(DependencyBuilder.newBuilder()
+                                                .withGroupId("default-group")
+                                                .withArtifactId("dependency-artifact")
+                                                .withVersion("0.9.0")
+                                                .withScope(SCOPE_COMPILE)
+                                                .withType("jar")
+                                                .withClassifier("default")
+                                                .build()));
+                                    }
+                                });
                             }
-                        });
+                        };
+                        setProject(project);
+
+                        session = mockMavenSession();
                     }
                 };
-                setProject(project);
-
-                session = mockMavenSession();
-            }
-        };
     }
 
     @Test
@@ -99,26 +83,18 @@ public class UseLatestReleasesMojoTest {
             throws MojoExecutionException, XMLStreamException, MojoFailureException, IllegalAccessException,
                     VersionRetrievalException {
         setVariableValueToObject(mojo, "processDependencies", true);
-        setVariableValueToObject(mojo, "allowSnapshots", false);
         setVariableValueToObject(mojo, "allowMajorUpdates", false);
         setVariableValueToObject(mojo, "allowMinorUpdates", true);
         setVariableValueToObject(mojo, "allowIncrementalUpdates", false);
 
-        try (MockedStatic<PomHelper> pomHelper = mockStatic(PomHelper.class)) {
-            pomHelper
-                    .when(() -> PomHelper.setDependencyVersion(any(), any(), any(), any(), any(), any(), any()))
-                    .thenReturn(true);
-            pomHelper
-                    .when(() -> PomHelper.getRawModel(any(MavenProject.class)))
-                    .thenReturn(mojo.getProject().getModel());
-            mojo.update(null);
-        }
+        tryUpdate();
         assertThat(changeRecorder.getChanges(), Matchers.empty());
     }
 
     @Test
     public void testAllowDowngrade()
-            throws MojoExecutionException, XMLStreamException, MojoFailureException, VersionRetrievalException {
+            throws MojoExecutionException, XMLStreamException, MojoFailureException, VersionRetrievalException,
+                    IllegalAccessException {
         mojo.repositorySystem = mockAetherRepositorySystem(new HashMap<String, String[]>() {
             {
                 put("artifactA", new String[] {"1.0.0", "1.0.1-SNAPSHOT"});
@@ -130,18 +106,9 @@ public class UseLatestReleasesMojoTest {
                         .withArtifactId("artifactA")
                         .withVersion("1.0.1-SNAPSHOT")
                         .build()));
-        mojo.allowDowngrade = true;
+        setVariableValueToObject(mojo, "allowDowngrade", true);
 
-        try (MockedStatic<PomHelper> pomHelper = mockStatic(PomHelper.class)) {
-            pomHelper
-                    .when(() -> PomHelper.setDependencyVersion(
-                            any(), anyString(), anyString(), anyString(), anyString(), any(Model.class), any()))
-                    .thenReturn(true);
-            pomHelper
-                    .when(() -> PomHelper.getRawModel(any(MavenProject.class)))
-                    .thenReturn(mojo.getProject().getModel());
-            mojo.update(null);
-        }
+        tryUpdate();
         assertThat(
                 changeRecorder.getChanges(),
                 hasItem(new DefaultDependencyVersionChange(
@@ -164,16 +131,7 @@ public class UseLatestReleasesMojoTest {
                         .withVersion("1.0.1-SNAPSHOT")
                         .build()));
 
-        try (MockedStatic<PomHelper> pomHelper = mockStatic(PomHelper.class)) {
-            pomHelper
-                    .when(() -> PomHelper.setDependencyVersion(
-                            any(), anyString(), anyString(), anyString(), anyString(), any(Model.class), any()))
-                    .thenReturn(true);
-            pomHelper
-                    .when(() -> PomHelper.getRawModel(any(MavenProject.class)))
-                    .thenReturn(mojo.getProject().getModel());
-            mojo.update(null);
-        }
+        tryUpdate();
         assertThat(changeRecorder.getChanges(), empty());
     }
 }

versions-maven-plugin/src/main/java/org/codehaus/mojo/versions/AbstractVersionsDependencyUpdaterMojo.java
@@ -23,6 +23,7 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.regex.Pattern;
 
 import org.apache.commons.lang3.StringUtils;
@@ -228,15 +229,12 @@ public abstract class AbstractVersionsDependencyUpdaterMojo extends AbstractVers
      * @since 1.0-alpha-3
      */
     protected Artifact findArtifact(Dependency dependency) {
-        if (getProject().getDependencyArtifacts() == null) {
-            return null;
-        }
-        for (Artifact artifact : getProject().getDependencyArtifacts()) {
-            if (compare(artifact, dependency)) {
-                return artifact;
-            }
-        }
-        return null;
+        return Optional.ofNullable(getProject().getDependencyArtifacts())
+                // no mutation, so parallelStream is safe
+                .flatMap(artifacts -> artifacts.parallelStream()
+                        .filter(artifact -> compare(artifact, dependency))
+                        .findAny())
+                .orElse(null);
     }
 
     /**

versions-maven-plugin/src/main/java/org/codehaus/mojo/versions/AbstractVersionsUpdaterMojo.java
@@ -123,13 +123,7 @@ public abstract class AbstractVersionsUpdaterMojo extends AbstractMojo {
     @Parameter(property = "generateBackupPoms", defaultValue = "true")
     protected boolean generateBackupPoms;
 
-    /**
-     * Whether to allow snapshots when searching for the latest version of an artifact.
-     *
-     * @since 1.0-alpha-1
-     */
-    @Parameter(property = "allowSnapshots", defaultValue = "false")
-    protected boolean allowSnapshots;
+    protected abstract boolean isAllowSnapshots();
 
     /**
      * Our versions helper.
@@ -296,7 +290,7 @@ public abstract class AbstractVersionsUpdaterMojo extends AbstractMojo {
     protected ArtifactVersion findLatestVersion(
             Artifact artifact, VersionRange versionRange, Boolean allowingSnapshots, boolean usePluginRepositories)
             throws MojoExecutionException, VersionRetrievalException {
-        boolean includeSnapshots = allowingSnapshots != null ? allowingSnapshots : this.allowSnapshots;
+        boolean includeSnapshots = allowingSnapshots != null ? allowingSnapshots : isAllowSnapshots();
         final ArtifactVersions artifactVersions =
                 getHelper().lookupArtifactVersions(artifact, versionRange, usePluginRepositories);
         return artifactVersions.getNewestVersion(versionRange, null, includeSnapshots, false);
@@ -437,7 +431,7 @@ public abstract class AbstractVersionsUpdaterMojo extends AbstractMojo {
         ArtifactVersion winner = version.getNewestVersion(
                 currentVersion,
                 property,
-                this.allowSnapshots,
+                isAllowSnapshots(),
                 this.reactorProjects,
                 this.getHelper(),
                 allowDowngrade,

versions-maven-plugin/src/main/java/org/codehaus/mojo/versions/CompareDependenciesMojo.java
@@ -116,6 +116,12 @@ public class CompareDependenciesMojo extends AbstractVersionsDependencyUpdaterMo
     @Parameter(property = "reportOutputFile")
     protected File reportOutputFile;
 
+    @Override
+    protected boolean isAllowSnapshots() {
+        // this parameter is used by base class, but shouldn't affect comparison; setting to true
+        return true;
+    }
+
     /**
      * The (injected) instance of {@link ProjectBuilder}
      *

versions-maven-plugin/src/main/java/org/codehaus/mojo/versions/DisplayDependencyUpdatesMojo.java
@@ -310,6 +310,19 @@ public class DisplayDependencyUpdatesMojo extends AbstractVersionsDisplayMojo {
     @Parameter(property = "pluginManagementDependencyExcludes")
     private List<String> pluginManagementDependencyExcludes;
 
+    /**
+     * Whether to allow snapshots when searching for the latest version of an artifact.
+     *
+     * @since 1.0-alpha-1
+     */
+    @Parameter(property = "allowSnapshots", defaultValue = "false")
+    protected boolean allowSnapshots;
+
+    @Override
+    protected boolean isAllowSnapshots() {
+        return allowSnapshots;
+    }
+
     // --------------------- GETTER / SETTER METHODS ---------------------
 
     @Inject

versions-maven-plugin/src/main/java/org/codehaus/mojo/versions/DisplayExtensionUpdatesMojo.java
@@ -145,6 +145,19 @@ public class DisplayExtensionUpdatesMojo extends AbstractVersionsDisplayMojo {
     @Parameter(property = "verbose", defaultValue = "false")
     private boolean verbose;
 
+    /**
+     * Whether to allow snapshots when searching for the latest version of an artifact.
+     *
+     * @since 1.0-alpha-1
+     */
+    @Parameter(property = "allowSnapshots", defaultValue = "false")
+    protected boolean allowSnapshots;
+
+    @Override
+    protected boolean isAllowSnapshots() {
+        return allowSnapshots;
+    }
+
     @Inject
     public DisplayExtensionUpdatesMojo(
             ArtifactHandlerManager artifactHandlerManager,

versions-maven-plugin/src/main/java/org/codehaus/mojo/versions/DisplayParentUpdatesMojo.java
@@ -131,6 +131,19 @@ public class DisplayParentUpdatesMojo extends AbstractVersionsDisplayMojo {
     @Parameter(property = "allowIncrementalUpdates", defaultValue = "true")
     protected boolean allowIncrementalUpdates = true;
 
+    /**
+     * Whether to allow snapshots when searching for the latest version of an artifact.
+     *
+     * @since 1.0-alpha-1
+     */
+    @Parameter(property = "allowSnapshots", defaultValue = "false")
+    protected boolean allowSnapshots;
+
+    @Override
+    protected boolean isAllowSnapshots() {
+        return allowSnapshots;
+    }
+
     // -------------------------- OTHER METHODS --------------------------
 
     @Inject

commit 375fe00680991eb61261bc00b25c0ea5c765b466

Apply excludes/includes to candidate versions, not just the current one
getUpdates() in UseLatestVersionsMojoBase called isIncluded() exactly
once per dependency, against the artifact's *current* version, before
fetching the list of newer candidate versions. A version-specific
exclude pattern (e.g. "groupId:artifactId:*:*:4*") therefore only ever
matched the current version -- if that didn't match, every candidate
version passed through unfiltered, including ones the pattern was
meant to reject.

This is a regression from #1202, which removed the old
getLastFiltered() method that used to build an Artifact per candidate
version and run it through isIncluded(). The shared getUpdates() path
never got an equivalent replacement.

Restore the per-candidate check: after filtering the candidate stream
with artifactVersionsFilter, also build an Artifact for each candidate
(same groupId/artifactId/type/classifier/scope as the current
dependency, but with the candidate's version) and run it through
isIncluded() too.

Fixes #1378

versions-maven-plugin/src/main/java/org/codehaus/mojo/versions/UseLatestVersionsMojoBase.java
@@ -299,7 +299,20 @@ public abstract class UseLatestVersionsMojoBase extends AbstractVersionsDependen
                                                             unchangedSegment,
                                                             getAllowSnapshots(),
                                                             getAllowDowngrade()))
-                                                    .filter(this::artifactVersionsFilter))
+                                                    .filter(this::artifactVersionsFilter)
+                                                    // isIncluded() above only checked the artifact's *current*
+                                                    // version; excludes/includes patterns targeting a candidate
+                                                    // version (e.g. "groupId:artifactId:*:*:4*") were silently
+                                                    // never applied after #1202 removed the old per-candidate
+                                                    // getLastFiltered() method -- restore that check here (gh-1378).
+                                                    .filter(ver -> isIncluded(artifactFactory.createArtifact(
+                                                            artifact.getGroupId(),
+                                                            artifact.getArtifactId(),
+                                                            ver.toString(),
+                                                            artifact.getType(),
+                                                            artifact.getClassifier(),
+                                                            artifact.getScope(),
+                                                            artifact.isOptional()))))
                                             .ifPresent(ver -> updates.add(
                                                     new DependencyVersionChange(changeKind, dep, ver.toString())));
                                 } catch (VersionRetrievalException

versions-maven-plugin/src/test/java/org/codehaus/mojo/versions/UseLatestReleasesMojoTest.java
@@ -17,6 +17,7 @@ package org.codehaus.mojo.versions;
 
 import javax.xml.stream.XMLStreamException;
 
+import java.util.Arrays;
 import java.util.HashMap;
 
 import org.apache.maven.model.Model;
@@ -39,6 +40,7 @@ import static org.codehaus.mojo.versions.utils.MockUtils.mockMavenSession;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.empty;
 import static org.hamcrest.Matchers.hasItem;
+import static org.hamcrest.Matchers.not;
 import static org.mockito.Mockito.mock;
 
 public class UseLatestReleasesMojoTest extends UseLatestVersionsMojoTestBase {
@@ -132,5 +134,39 @@ public class UseLatestReleasesMojoTest extends UseLatestVersionsMojoTestBase {
 
         tryUpdate();
         assertThat(changeRecorder.getChanges(), empty());
+    }
+
+    @Test
+    public void testExcludeFilterAppliesToCandidateVersion()
+            throws MojoExecutionException, XMLStreamException, MojoFailureException, IllegalAccessException,
+                    VersionRetrievalException {
+        mojo.getProject()
+                .getModel()
+                .setDependencies(Arrays.asList(
+                        DependencyBuilder.newBuilder()
+                                .withGroupId("default-group")
+                                .withArtifactId("dependency-artifact")
+                                .withVersion("0.9.0")
+                                .withType("pom")
+                                .withClassifier("default")
+                                .withScope(SCOPE_COMPILE)
+                                .build(),
+                        DependencyBuilder.newBuilder()
+                                .withGroupId("default-group")
+                                .withArtifactId("other-artifact")
+                                .withVersion("1.0")
+                                .withType("pom")
+                                .withClassifier("default")
+                                .withScope(SCOPE_COMPILE)
+                                .build()));
+        setVariableValueToObject(mojo, "processDependencies", true);
+        // The current version (1.0) does not match this pattern, but the only
+        // available newer release (2.0) does -- gh-1378.
+        setVariableValueToObject(mojo, "excludes", new String[] {"default-group:other-artifact:*:*:2*"});
+
+        tryUpdate();
+        assertThat(
+                changeRecorder.getChanges(),
+                not(hasItem(new DefaultDependencyVersionChange("default-group", "other-artifact", "1.0", "2.0"))));
     }
 }